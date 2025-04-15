f6d0bdbcdf6a72ce306500f223fa271da696ec1a
Dimas Alif Ferdiansyah <135016020+dmasaliff@users.noreply.github.com>
2025-04-15 20:30:24 +0700

    Add files via upload

---
+++ PROJECT SBD DAN PSBD/index.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+// Contoh username dan password yang valid
+$valid_username = 'admin';
+$valid_password = '12345678';
+
+$error = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if ($username === $valid_username && $password === $valid_password) {
+        $_SESSION['username'] = $username;
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <?php if ($error): ?>
+        <p style="color: red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+    <form method="post" action="">
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
